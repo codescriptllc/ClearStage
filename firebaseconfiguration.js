4db8145f1afd82dff3fba8ff67a469ad545b8ef5
codescriptllc <155702502+codescriptllc@users.noreply.github.com>
2025-06-30 22:23:53 -0700

    Add files via upload

---
+++ firebaseconfiguration.js
@@ -0,0 +1,13 @@
+    const firebaseConfig = {
+      apiKey: "AIzaSyA0J3XwYlan8aR5shsOw0nrFf0JwpBNQXs",
+      authDomain: "clearstage-73714.firebaseapp.com",
+      projectId: "clearstage-73714",
+      storageBucket: "clearstage-73714.firebasestorage.app",
+      messagingSenderId: "340372540118",
+      appId: "1:340372540118:web:7bd38459b6884b3c8a5777",
+      measurementId: "G-D3KNTT92M1"
+    };
+
+firebase.initializeApp(firebaseConfig);
+
+
